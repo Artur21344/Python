093d31e9511a09841f91946ebf69fed1d59a5908
Artur21344 <forceartur21@gmail.com>
2025-10-25 13:48:01 +0300

    just practicing

---
+++ main.py
@@ -0,0 +1,8 @@
+print("Hello, World!")
+name = input("What is your name? ")
+print(f"Nice to meet you, {name}!")
+age = input("How old are you? ")
+if age < '18':
+    print("You're quite young!")
+else:
+    print("Age is just a number!")
