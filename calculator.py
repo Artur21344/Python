d0590c941bca9425e05223322e807c6f34c91d58
Artur21344 <forceartur21@gmail.com>
2025-10-31 17:33:32 +0200

    цикл while

--- calculator.py
+++ calculator.py
@@ -4,7 +4,7 @@ print(f"Радий познайомитись, {name}!")
 print('мій калькулятор допоможе тобі з простими обчисленнями')
 num1 = int(input("Введи перше число: "))
 num2 = int(input("Введи друге число: "))
-operation = input("Введи операцію (+, -, *, /): ")
+operation = input("Введи операцію (+, -, *, /, **): ")
 
 if operation == "+":
     result = num1 + num2
@@ -18,4 +18,29 @@ else:
     result = "Невідома операція"
 
 print(f"Результат: {result}")
+
+while True:
+    again = input("Бажаєш виконати ще одну операцію? (так/ні): ").lower()
+    if again == "так":
+        num1 = int(input("Введи перше число: "))
+        num2 = int(input("Введи друге число: "))
+        operation = input("Введи операцію (+, -, *, /, **): ")
+
+        if operation == "+":
+            result = num1 + num2
+        elif operation == "-":
+            result = num1 - num2
+        elif operation == "*":
+            result = num1 * num2
+        elif operation == "/":
+            result = num1 / num2
+        else:
+            result = "Невідома операція"
+
+        print(f"Результат: {result}")
+    elif again == "ні":
+        print(f"До побачення, {name}!")
+        break
+    else:
+        print("Будь ласка, відповідай 'так' або 'ні'.")
 
