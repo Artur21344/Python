91733e2bf98fec776506b63b3b2c08c6cdc7a3e9
Artur21344 <forceartur21@gmail.com>
2025-11-22 14:57:08 +0200

    up

--- calculator.py
+++ calculator.py
@@ -14,38 +14,44 @@ def invalid_operation():
 
 from unittest import result
 
-
-print("Вітаю в калькуляторі!")
-name = input("Як тебе звати? ")
-print(f"Радий познайомитись, {name}!")
-print('мій калькулятор допоможе тобі з простими обчисленнями')
-num1 = int(input("Введи перше число: "))
 while True:
-    if num1 >= 0:
-        break
+    print("Вітаю в калькуляторі!")
+    name = input("Як тебе звати? ")
+    print(f"Радий познайомитись, {name}!")
+    print('мій калькулятор допоможе тобі з простими обчисленнями')
+    num1 = int(input("Введи перше число: "))
+    while True:
+        if num1 >= 0:
+            break
+        else:
+            print("Будь ласка, введи додатнє число.")
+            num1 = int(input("Введи перше число: "))
+    num2 = int(input("Введи друге число: "))
+    while True:
+        if num2 >= 0:
+            break
+        else:
+            print("Будь ласка, введи додатнє число.")
+            num2 = int(input("Введи друге число: "))
+    operation = input("Введи операцію (+, -, *, /, **): ")
+    print("Результат: ")
+    if operation == '+':
+        print(plus(num1, num2))
+    elif operation == '-':
+        print(minus(num1, num2))
+    elif operation == '*':
+        print(multiply(num1, num2))
+    elif operation == '/':
+        print(divide(num1, num2))
+    elif operation == '**':
+        print(square(num1, num2))
     else:
-        print("Будь ласка, введи додатнє число.")
-        num1 = int(input("Введи перше число: "))
-num2 = int(input("Введи друге число: "))
-while True:
-    if num2 >= 0:
-        break
-    else:
-        print("Будь ласка, введи додатнє число.")
-        num2 = int(input("Введи друге число: "))
-operation = input("Введи операцію (+, -, *, /, **): ")
-print("Результат: ")
-if operation == '+':
-    print(plus(num1, num2))
-elif operation == '-':
-    print(minus(num1, num2))
-elif operation == '*':
-    print(multiply(num1, num2))
-elif operation == '/':
-    print(divide(num1, num2))
-elif operation == '**':
-    print(square(num1, num2))
-else:
-    print(invalid_operation())
-print("Дякую, що скористався моїм калькулятором, до побачення!")
+        print(invalid_operation())
+    print("Дякую, що скористався моїм калькулятором, до побачення!")
 
+    answer = input("Хочеш зробити ще одне обчислення? (так/ні): ")
+    if answer == 'так':
+        continue
+    else:
+        print("До побачення!")
+    break
