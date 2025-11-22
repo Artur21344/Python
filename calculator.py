28ad20df1079a41c2eb196596dc262bbdabcf5ed
Artur21344 <forceartur21@gmail.com>
2025-11-22 14:23:51 +0200

    function update

--- calculator.py
+++ calculator.py
@@ -1,45 +1,44 @@
+from unittest import result
+
+
 print("Вітаю в калькуляторі!")
 name = input("Як тебе звати? ")
 print(f"Радий познайомитись, {name}!")
 print('мій калькулятор допоможе тобі з простими обчисленнями')
 num1 = int(input("Введи перше число: "))
+while True:
+    if num1 >= 0:
+        break
+    else:
+        print("Будь ласка, введи додатнє число.")
+        num1 = int(input("Введи перше число: "))
 num2 = int(input("Введи друге число: "))
+while True:
+    if num2 >= 0:
+        break
+    else:
+        print("Будь ласка, введи додатнє число.")
+        num2 = int(input("Введи друге число: "))
 operation = input("Введи операцію (+, -, *, /, **): ")
 
-if operation == '+':
-    result = num1 + num2
-elif operation == '-':
-    result = num1 - num2
-elif operation == '*':
-    result = num1 * num2
-elif operation == '/':
-    result = num1 / num2
-elif operation == '**':
-    result = num1 ** num2
-else:
+def plus(a, b):
+    return a + b
+result = plus(num1, num2)
+def minus(a, b):
+    return a - b
+result = minus(num1, num2)
+def multiply(a, b):
+    return a * b
+result = multiply(num1, num2)
+def divide (a, b):
+    return a / b
+result = divide(num1, num2)
+def square(a, b):
+    return a ** b
+result = square(num1, num2)
+def invalid_operation():    
     result = "Невідома операція"
-while True:
-    print("Бажаєш виконати ще одну операцію?")
-    again = input("Введи 'так' для продовження або 'ні' для виходу: ").lower()
-    if again == 'так':
-        num1 = int(input("Введи перше число: "))
-        num2 = int(input("Введи друге число: "))
-        operation = input("Введи операцію (+, -, *, /, **): ")
 
-        if operation == '+':
-            result = num1 + num2
-        elif operation == '-':
-            result = num1 - num2
-        elif operation == '*':
-            result = num1 * num2
-        elif operation == '/':
-            result = num1 / num2
-        elif operation == '**':
-            result = num1 ** num2
-        else:
-            result = "Невідома операція"
-    elif again == 'ні':
-        break
 print(f"Результат: {result}")
 print("Дякую, що скористався моїм калькулятором, до побачення!")
 
