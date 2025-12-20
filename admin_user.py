9d20c05b6d39ea4a1bc876d6e03d592816082cd6
Artur21344 <forceartur21@gmail.com>
2025-12-20 15:14:21 +0200

    try 2

--- admin_user.py
+++ admin_user.py
@@ -5,16 +5,19 @@ class User:
 class Admin(User):
     def main(self):
         return f"Admin: {self.name}"
-
+def password_do(admin):
+    return f"Admin {admin.name} changed password."
 def block_user(user):
     return f"User {user.name} has been blocked."
+
+def hello_user(user):
+    return f"Hello i am {user.__class__.__name__} my name is {user.name}"
 
 admin1 = Admin("Artur")
 print(admin1.main())
 
 User1 = User("Bob")
-print(User1.name)
-print(f"User: {User1.name} hello")
+print(hello_user(User1))
 
 print(Admin("Artur").main())
-print(f"Hello {admin1.name}")
+print(hello_user(admin1))
