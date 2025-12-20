058ab9ed8ddebf87a0841ebb91a9649e678bd389
Artur21344 <forceartur21@gmail.com>
2025-12-20 15:35:58 +0200

    try 4

--- admin_user.py
+++ admin_user.py
@@ -1,14 +1,16 @@
 class User:
-    def __init__(self,name):
+    def __init__(self,name, class_name = "User"):
         self.name = name
-    
+        self.class_name = class_name
+
     def hello(self):
-        return f"Hello, {self.name}!"
-    
-    def get_role(self):
-        return "User"
+        return f"Hello, I am {self.name} i am {self.class_name}"
+
 
 class Admin(User):
+    def __init__(self, name):
+        super().__init__(name, class_name="Admin")
+        
     def get_role(self):
         return "Admin"
 
@@ -20,4 +22,4 @@ print(admin1.get_role())
 
 User1 = User("Bob")
 print(User1.hello())
-print(User1.get_role())
+print(User1.class_name)
