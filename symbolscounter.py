7079a418d68b4ad7df872e49d80ff8e67d5f859b
Artur21344 <forceartur21@gmail.com>
2026-01-10 15:53:06 +0200

    def

--- symbolscounter.py
+++ symbolscounter.py
@@ -1,19 +1,24 @@
+def count_characters(text):
+    length = 0
+    letters = 0
+    digits = 0
+    other = 0
+    
+    for char in text:
+        length += 1
+        
+        if char.isalpha():
+            letters += 1
+        elif char.isdigit():
+            digits += 1
+        else:
+            other += 1
+    
+    return length, letters, digits, other
+
 user_input = input("Введіть рядок: ")
 
-length = 0
-letters = 0
-digits = 0
-other = 0
-
-for char in user_input:
-    length += 1
-    
-    if char.isalpha():
-        letters += 1
-    elif char.isdigit():
-        digits += 1
-    else:
-        other += 1
+length, letters, digits, other = count_characters(user_input)
 
 print("\n--- Результати ---")
 print(f"Довжина рядка: {length}")
